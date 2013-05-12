488c6e986e53b44bd39907ffc95c0b760f24ecc7
Jordon Bedwell <envygeeks@gmail.com>
2013-05-12 14:32:51 -0500

    Fix bad Coveralls addition.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,16 @@
-require 'simplecov'
+require "coveralls"
+require "simplecov"
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+]
+
+Coveralls.noisy = true
 SimpleCov.command_name "Unit Tests"
 SimpleCov.start do
   add_filter "/test/"
   add_filter "/demo/"
 end
-
-require 'coveralls'
-Coveralls.wear!
 
 require 'minitest/autorun'
