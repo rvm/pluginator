3b198123a2d4d139e681974d543d8c92b732045e
Michal Papis <mpapis@gmail.com>
2014-10-20 12:15:56 +0200

    fix docs param name for Conversions.class2string

--- lib/plugins/pluginator/extensions/conversions.rb
+++ lib/plugins/pluginator/extensions/conversions.rb
@@ -22,8 +22,8 @@ module Pluginator::Extensions
   module Conversions
 
     # converts class name to a file name
-    # @param str [String] class like string
-    # @return    [String] file like string
+    # @param klass [String] class like string
+    # @return      [String] file like string
     def class2string( klass )
       klass.to_s.gsub(/([A-Z])/m){|match| "_#{$1.downcase}" }[1..-1]
     end
